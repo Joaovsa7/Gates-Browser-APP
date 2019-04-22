01485b1bc10ef99e7393140830fb2028fd49473f
Joao <jvsadesign@gmail.com>
2019-04-22 20:49:40 -0300

    Inicio do projeto

---
+++ app/src/main/java/br/com/klenne/gates_app/Launch.kt
@@ -0,0 +1,12 @@
+package br.com.klenne.gates_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Launch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch2)
+    }
+}
